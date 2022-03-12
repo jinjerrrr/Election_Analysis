da09e66d82ac0aa5c6d17798502731dd2618ac05
jinjerrrr <96097264+jinjerrrr@users.noreply.github.com>
2022-03-12 11:17:08 -0800

    Update PyPoll_challenge_starter_code.py

--- PyPoll_challenge_starter_code.py
+++ PyPoll_challenge_starter_code.py
@@ -6,9 +6,9 @@ import csv
 import os
 
 # Add a variable to load a file from a path.
-file_to_load = "C:\\Users\\Arid_\\Desktop\\Election_Analysis\\Resources\\election_results.csv"
+file_to_load = os.path.join("Election_Analysis","Resources", "election_results.csv")
 # Add a variable to save the file to a path.
-file_to_save = "C:\\Users\\Arid_\\Desktop\\Election_Analysis\\analysis\\elction_results.txt"
+file_to_save =os.path.join("Election_Analysis","Resources", "election_results.txt")
 
 # Initialize a total vote counter.
 total_votes = 0
